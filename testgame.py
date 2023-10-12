5dfde3349db474888b5dad3611a8fa0f416b18a2
KarlMental <karl.e.andersson@gmail.com>
2023-10-12 22:29:02 +0200

    added a guy and some movement

--- testgame.py
+++ testgame.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
@@ -12,13 +13,46 @@ def main():
     
     screen.blit(background, (0, 0))
     pygame.display.flip()
+    character = {
+        'x': screen.get_width()/2,
+        'y': screen.get_height()/2,
+        'width': 30,
+        'height': 60,
+        'speed': 5,
+        'color': (255, 0, 0)
+    }
     
     while 1:
+        pygame.time.delay(100)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 return
+        
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            character['x'] -= character['speed']
+        if keys[pygame.K_RIGHT]:
+            character['x'] += character['speed']
+        if keys[pygame.K_UP]:
+            character['y'] -= character['speed']
+        if keys[pygame.K_DOWN]:
+            character['y'] += character['speed']
+        if keys[pygame.K_a]:
+            character['width'] += 1
+        if keys[pygame.K_d]:
+            character['width'] -= 1
+        if keys[pygame.K_s]:
+            character['height'] -= 1
+        if keys[pygame.K_w]:
+            character['height'] += 1
+
+        screen.fill((250, 250, 250))
+        pygame.draw.rect(screen, character['color'], (character['x'], character['y'], character['width'], character['height']))
+        pygame.display.flip()
+
 
 
 
